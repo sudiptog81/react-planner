a62ad5d4db29c4befd52ad4e063aedcf83edcfd5
Sudipto <sudiptog81@gmail.com>
2018-12-27 09:12:37 +0530

    tests fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import rootReducer from "./store/reducers/rootReducer";
+import firebase from "./config/firebase";
+// eslint-disable-next-line no-unused-vars
+import ReactGA from "react-ga";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reactReduxFirebase(firebase, {
+      attachAuthIsReady: true,
+      useFirestoreForProfile: true,
+      userProfile: "users"
+    }),
+    reduxFirestore(firebase)
+  )
+);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
